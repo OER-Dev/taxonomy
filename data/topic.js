945d853827a4f1c72ad0167c04bae551bd81df2e
Christer Gundersen <46853403+christer-io@users.noreply.github.com>
2021-10-18 20:44:31 +0200

    Added resources for three math topics

--- data/topic.js
+++ data/topic.js
@@ -1,34 +1,34 @@
 export const resources = [{
         "language": "en",
         "order": 0,
-        "mainTopic": "math",
+        "mainTopic": "early-math",
         "topic": "counting",
         "topicTitle": "Counting",
         "illustration": "url",
-        "badge": "url",
+        "badge": "https://storage.googleapis.com/edupix-strapi/count_with_small_numbers_2x_378d9a6302/count_with_small_numbers_2x_378d9a6302.png",
         "points": 100,
-        "levelUp": "https://obione.io/game/counting-10/?play=1"
+        "levelUp": "https://obione.io/game/put-numbers-in-the-correct-order/?play=1"
     },
     {
         "language": "en",
         "order": 1,
-        "mainTopic": "math",
-        "topic": "counting-in-order",
-        "topicTitle": "Counting in order",
+        "mainTopic": "early-math",
+        "topic": "counting-objects",
+        "topicTitle": "Counting objects",
         "illustration": "url",
-        "badge": "url",
+        "badge": "https://storage.googleapis.com/edupix-strapi/count_in_order_21ed7e3732/count_in_order_21ed7e3732.png",
         "points": 100,
-        "levelUp": "https://digitallibrary.io/game/count-in-order/?play=1"
+        "levelUp": "https://obione.io/game/counting-objects-level-up/?play=1"
     },
     {
         "language": "en",
         "order": 2,
-        "mainTopic": "math",
-        "topic": "0-120",
-        "topicTitle": "Numbers 0 - 120",
+        "mainTopic": "early-math",
+        "topic": "comparing-numbers",
+        "topicTitle": "Comparing small numbers",
         "illustration": "url",
-        "badge": "url",
+        "badge": "https://storage.googleapis.com/edupix-strapi/compare_small_numbers_92e99923d6/compare_small_numbers_92e99923d6.png",
         "points": 100,
-        "levelUp": "https://digitallibrary.io/game/missing-numbers-to-120/"
+        "levelUp": "https://obione.io/game/comparing-small-numbers/?play=1"
     }
 ]
